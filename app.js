9ab18801f042f03fb7c832b6491c06b4979b4ed9
Sai Htet Aung <saihtetaung276@gmail.com>
2021-09-19 13:10:23 +0630

    'Hello'

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.json());
 
 // static middlewares
 app.use("/public", express.static(__dirname+"/public"));
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/public'));
 
 // routers
 app.use("/", indexRouter);
