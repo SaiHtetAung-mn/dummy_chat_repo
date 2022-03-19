0c298bbac233e812425342514ce99131fb50406c
Sai Htet Aung <66723382+SaiHtetAung-mn@users.noreply.github.com>
2022-03-19 20:51:26 +0630

    Update Mail.js

--- utils/Mail.js
+++ utils/Mail.js
@@ -13,7 +13,7 @@ const createTransporter = async () => {
         refresh_token: process.env.REFRESH_TOKEN
     });
 
-    /*const accessToken = await new Promise((resolve, reject) => {
+    const accessToken = await new Promise((resolve, reject) => {
         oauth2Client.getAccessToken((err, token) => {
             if(err) {
                 reject("Failed to create access token");
@@ -22,14 +22,14 @@ const createTransporter = async () => {
                 resolve(token);
             }
         })
-    });*/
+    });
 
     const transporter = nodeMailer.createTransport({
         service: "gmail",
         auth: {
             type: "OAuth2",
             user: process.env['GMAIL_NAME'],
-            accessToken: process.env.ACCESS_TOKEN,
+            accessToken,
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
             refreshToken: process.env.REFRESH_TOKEN
